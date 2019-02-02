c1c5e140692898414cf9ddcda870a187ccd31685
Kris12301 <47269424+Kris12301@users.noreply.github.com>
2019-02-02 10:49:26 +0200

    Add files via upload

---
+++ Program.cs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            var n = int.Parse(Console.ReadLine());
+            var s = 1.0;
+        for (int i=2;i<=n;i++)
+            {
+                s += (i*i)/(i-1.0);
+            }
+            Console.WriteLine("{0:N2}",s);
+        }
+    }
+}
